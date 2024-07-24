a845451c66a65b60d9c8d6b60fa7d9d4105e5e4d
adamswonder <adamswill.i.am71002@gmail.com>
2024-07-24 23:09:26 +0300

    Updates to show ETH balance

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,21 +1,39 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import { ethers } from 'ethers';
 import useAuth from '../hooks/useAuth'
 import useUser from '../hooks/useUser';
 
 export default function Home() {
     const { user } = useAuth();
     const getUser = useUser()
+    const [ethBalance, setEthBalance] = useState(null)
 
     useEffect(() => {
         getUser()
     }, [])
+
+    useEffect(() => {
+        async function fetchEthBalance() {
+            if (user?.ethereum_wallet_address) {
+                const apiKey = process.env.REACT_APP_ETHERSCAN_API_KEY
+                const provider = new ethers.providers.EtherscanProvider(null, apiKey)
+                const balance = await provider.getBalance(user.ethereum_wallet_address)
+                setEthBalance(ethers.utils.formatEther(balance))
+            }
+        }
+
+        fetchEthBalance()
+    }, [user])
 
     return (
         <div className='container mt-3'>
             <h2>
                 <div className='row'>
                     <div className="mb-12">
-                        {user?.email !== undefined ? 'List user Ethereum balance' : 'Please login first'}
+                        {user?.email !== undefined ? 
+                            <div>
+                                <p>Ethereum Wallet Balance: {ethBalance !== null ? `${ethBalance} ETH` : 'Fetching balance...'}</p>
+                            </div> : 'Please login first'}
                     </div>
                 </div>
             </h2>
